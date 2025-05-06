2de05bc1388eea7cf395150b435ec7f6fb90413b
Aldin Philo <aldinphilo@gmail.com>
2025-05-06 22:37:41 +0530

    Delete Functionality Added

--- src/gmsheet/OpenGM.jsx
+++ src/gmsheet/OpenGM.jsx
@@ -288,12 +288,20 @@ const OpenGM = ({ refreshlst, projectId, accountId, sowId, accountdata }) => {
     }
 
     const handleSaveAll = () => {
-        if(isRunSheetSave){
+        if (isRunSheetSave) {
             handleSaveRunSheet();
         }
-        if(isGmSheetSave){
+        if (isGmSheetSave) {
             handleSaveGmSheet();
-        }        
+        }
+    }
+
+    const deleteRow = (index) => {
+        let deleterows = rows.filter((_, i) => index != i)
+        setRows(deleterows)
+        if (deleterows.length == 0) {
+            setIsGmSheetSave(false);
+        }
     }
 
     let showTextBox = 0;
@@ -546,7 +554,7 @@ const OpenGM = ({ refreshlst, projectId, accountId, sowId, accountdata }) => {
                                                 <th className='px-2 py-2 border border-gray-600' key={"TableHeader-" + iH}>{runsheetHeaders}</th>
                                             ))
                                         }
-                                        <th colSpan={4}></th>
+                                        <th colSpan={isGmSheetSave ? 5 : 4}></th>
                                     </tr>
                                     <tr className='whitespace-nowrap'>
                                         <th className='px-2 py-2 border border-gray-600'>BRSPD Mgr</th>
@@ -572,6 +580,7 @@ const OpenGM = ({ refreshlst, projectId, accountId, sowId, accountdata }) => {
                                         <th className='px-2 py-2 border border-gray-600'>Total Cost</th>
                                         <th className='px-2 py-2 border bg-blue-600 text-white border-gray-600'>TotalRevenue(YTD)</th>
                                         <th className='px-2 py-2 border bg-blue-600 text-white border-gray-600'>TotalRevenue(YTD+Project)</th>
+                                        {isGmSheetSave && <th className='px-2 py-2 border border-gray-600'></th>}
                                     </tr>
                                 </thead>
                                 <tbody className="divide-y divide-gray-600 bg-Black">
@@ -633,6 +642,7 @@ const OpenGM = ({ refreshlst, projectId, accountId, sowId, accountdata }) => {
                                                 <td className="p-2 border border-gray-600">{row.totalrevenue}</td>
                                                 <td className="p-2 border border-gray-600">{row.totalrevenueytd}</td>
                                                 <td className="p-2 border border-gray-600">{row.totalrevenueytdproject}</td>
+                                                {isGmSheetSave && <td className="p-2 border border-gray-600"></td>}
                                             </tr>
                                         ))
                                     }
@@ -723,6 +733,13 @@ const OpenGM = ({ refreshlst, projectId, accountId, sowId, accountdata }) => {
                                                 <td className="p-2 border border-gray-600">{row.totalrevenue}</td>
                                                 <td className="p-2 border border-gray-600">{row.totalrevenueytd}</td>
                                                 <td className="p-2 border border-gray-600">{row.totalrevenueytdproject}</td>
+                                                <td className="p-2 border border-gray-600">
+                                                    <button className='text-red-600 py-2 px-2 rounded hover:text-red-800 transition-colors' type='button' onClick={() => deleteRow(i)}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-8">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                        </svg>
+                                                    </button>
+                                                </td>
                                             </tr>
                                         ))
                                     }
